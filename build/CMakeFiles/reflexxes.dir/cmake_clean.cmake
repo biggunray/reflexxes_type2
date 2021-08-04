bd12e515a7f325ad35e225350e23341f08eaa34c
biggunray <rayzhang1997@yahoo.com>
2021-08-04 13:32:13 -0400

    update

---
+++ build/CMakeFiles/reflexxes.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reflexxes"
+  "CMakeFiles/reflexxes-complete"
+  "reflexxes-prefix/src/reflexxes-stamp/reflexxes-install"
+  "reflexxes-prefix/src/reflexxes-stamp/reflexxes-mkdir"
+  "reflexxes-prefix/src/reflexxes-stamp/reflexxes-download"
+  "reflexxes-prefix/src/reflexxes-stamp/reflexxes-update"
+  "reflexxes-prefix/src/reflexxes-stamp/reflexxes-patch"
+  "reflexxes-prefix/src/reflexxes-stamp/reflexxes-configure"
+  "reflexxes-prefix/src/reflexxes-stamp/reflexxes-build"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/reflexxes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/reflexxes_type2-config.cmake
@@ -0,0 +1,2 @@
+set(reflexxes_type2_INCLUDE_DIRS "/usr/local/include")
+set(reflexxes_type2_LIBRARIES "/usr/local/lib/libReflexxesTypeII.so")
